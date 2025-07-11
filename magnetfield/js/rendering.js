4ab4c161dd68db37a3bc6f1689912aff02c7fb47
hitsen090 <hitsen090@gmail.com>
2025-07-11 22:55:03 +0200

    Update rendering.js

--- magnetfield/js/rendering.js
+++ magnetfield/js/rendering.js
@@ -1,5 +1,5 @@
 function calculatePotential(x, y) {
-    /*let potential = 0;
+    let potential = 0;
     for (let i = 0; i < charges.length; i++) {
         const charge = charges[i];
         const dx = x - charge.x;
@@ -9,8 +9,8 @@ function calculatePotential(x, y) {
             potential += k * charge.q / r;
         }
     }
-    return potential;*/
-    return calculateField(x,y);
+    return potential;
+  //  return calculateField(x,y);
 }
 
 
