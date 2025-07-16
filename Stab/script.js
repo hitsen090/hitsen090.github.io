a11a965034f9be627d5e716700ec8ba3c4f2608c
hitsen090 <hitsen090@gmail.com>
2025-07-16 21:29:12 +0200

    Update script.js

--- Stab/script.js
+++ Stab/script.js
@@ -1,1 +1,26 @@
 //
+async function render() {
+    //performance.mark('renderStart');
+    if (isRendering) return;
+    isRendering = true;
+
+    clearTimeout(fpsTimeout);
+    if (!startTime) startTime = performance.now();
+
+    if (!SETTINGS.animatedMode) $('#progress').text(I18N[LANG].rendering);
+
+    let canvasWidth = canvas.width;
+    let canvasHeight = canvas.height;
+    //animateWithoutRequest();
+
+    let currTime = performance.now();
+    lastRenderTime = Math.round(currTime - startTime);
+    runningFPS = runningFPS * 0.8 + 200/lastRenderTime;
+   // updateJSI18N();
+
+    isRendering = false;
+    //performance.mark('renderEnd');
+    startTime = currTime;
+
+    fpsTimeout = setTimeout(e => startTime = null, 100);
+}
