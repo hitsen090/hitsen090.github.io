6663926a4c83d2276539eddc451b08ff5afaea5c
hitsen090 <hitsen090@gmail.com>
2025-07-19 19:27:32 +0200

    Update script.js

--- Stab/script.js
+++ Stab/script.js
@@ -63,8 +63,8 @@ async function render() {
       ctx.moveTo(0.5*width,0.75*height);
       let x2 = 0.5*width-0.25*height*Math.sin(e.data.alpha2);
       let y2 = 0.75*height-0.25*height*Math.cos(e.data.alpha2);
-      let x1 = x2 +0.25*height*Math.sin(e.data.alpha1);
-      let y1 = y2 + 0.25*height*Math.cos(e.data.alpha1);
+      let x1 = x2 -0.25*height*Math.sin(e.data.alpha1);
+      let y1 = y2 - 0.25*height*Math.cos(e.data.alpha1);
       ctx.lineTo(x2,y2);
       ctx.lineTo(x1,y1);
       //alert(9);
