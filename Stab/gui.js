fca7cd179c5f8b2432a0859a2bcd068bf7248965
hitsen090 <hitsen090@gmail.com>
2025-07-19 23:00:24 +0200

    Update gui.js

--- Stab/gui.js
+++ Stab/gui.js
@@ -43,14 +43,23 @@ let listeners = [
     }},
     {sel: "#l", type: "", event: "input",var: "l", def: "1", defVar: 1, callback: e=>{
         SETTINGS.l = parseFloat(e.target.value);
+        if(!SETTINGS.stop){
+        SETTINGS.stop = true;
+        render();}
         updateJSI18N();
     }},
     {sel: "#q", type: "", event: "input",var: "q", def: "1000", defVar: 1000, callback: e=>{
         SETTINGS.q = parseFloat(e.target.value);
+        if(!SETTINGS.stop){
+        SETTINGS.stop = true;
+        render();}
         updateJSI18N();
     }},
     {sel: "#k", type: "", event: "input",var: "k", def: "10000", defVar: 10000, callback: e=>{
         SETTINGS.k = parseFloat(e.target.value);
+        if(!SETTINGS.stop){
+        SETTINGS.stop = true;
+        render();}
         updateJSI18N();
     }},
     {sel: "#alpha", type: "", event: "input",var: "alpha", def: "0.2", defVar: 0.2, callback: e=>{
@@ -62,10 +71,16 @@ let listeners = [
     }},
     {sel: "#v", type: "", event: "input",var: "v", def: "10", defVar: 10, callback: e=>{
         SETTINGS.v = parseFloat(e.target.value);
+        if(!SETTINGS.stop){
+        SETTINGS.stop = true;
+        render();}
         updateJSI18N();
     }},
     {sel: "#dt", type: "", event: "input",var: "dt", def: "0.0001", defVar: 0.0001, callback: e=>{
         SETTINGS.dt = parseFloat(e.target.value);
+        if(!SETTINGS.stop){
+        SETTINGS.stop = true;
+        render();}
         updateJSI18N();
     }},
     {sel: "#slider_E", type: "slider", var: "arrowsSpacing", def: "20", defVar: 20, callback: e=>{
