e0cba4f11867d06bcdce731a8f6d7e3a22195969
hitsen090 <hitsen090@gmail.com>
2025-07-19 22:19:51 +0200

    Update gui.js

--- Stab/gui.js
+++ Stab/gui.js
@@ -53,7 +53,7 @@ let listeners = [
     {sel: "#alpha", type: "", event: "input",var: "alpha", def: "0.2", defVar: 0.2, callback: e=>{
         SETTINGS.alpha = parseFloat(e.target.value);
         updateJSI18N();
-        workers.terminate();
+        //workers.terminate();
         render();
     }},
     {sel: "#v", type: "", event: "input",var: "v", def: "10", defVar: 10, callback: e=>{
