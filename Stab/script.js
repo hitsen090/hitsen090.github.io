d0833137a44b4d1e0cdc48130b632cc24379100c
hitsen090 <hitsen090@gmail.com>
2025-07-19 18:33:07 +0200

    Update script.js

--- Stab/script.js
+++ Stab/script.js
@@ -1,5 +1,5 @@
 alert(77);
-document.documentElement.style.setProperty('--real-vh', `${window.innerHeight}px`);
+/*document.documentElement.style.setProperty('--real-vh', `${window.innerHeight}px`);
 alert(8);
 while(true){}
 let m = SETTINGS.q*SETTINGS.l/SETTINGS.g;
@@ -61,27 +61,5 @@ async function render() {
    };
    }catch(e){if(p<1){alert(e);p++;}}
     //performance.mark('renderStart');
-   /* if (isRendering) return;
-    isRendering = true;
-
-    clearTimeout(fpsTimeout);
-    if (!startTime) startTime = performance.now();
-
-    if (!SETTINGS.animatedMode) $('#progress').text(I18N[LANG].rendering);
-
-    let canvasWidth = canvas.width;
-    let canvasHeight = canvas.height;
-    //animateWithoutRequest();
-
-    let currTime = performance.now();
-    lastRenderTime = Math.round(currTime - startTime);
-    runningFPS = runningFPS * 0.8 + 200/lastRenderTime;
-   // updateJSI18N();
-
-    isRendering = false;
-    //performance.mark('renderEnd');
-    startTime = currTime;
-
-    fpsTimeout = setTimeout(e => startTime = null, 100);*/
     
-}
+}*/
