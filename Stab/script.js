01d351dd9ce8650545ca036383374be4f164180d
hitsen090 <hitsen090@gmail.com>
2025-08-04 22:08:20 +0200

    Update script.js

--- Stab/script.js
+++ Stab/script.js
@@ -27,8 +27,8 @@ async function render() {
               } = e.data;
      let w1 = 0;
      let w2 = 0;
-     let alpha2 = alpha;
-     let alpha1 = 0;
+     let alpha2 = 0;
+     let alpha1 = alpha;
      let M1 = 0;
      let M2 = 0;
      setInterval(() => {
