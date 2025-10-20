16da682440629ec27e84307e323d0d6f24c9f528
hitsen090 <hitsen090@gmail.com>
2025-10-20 17:36:16 +0200

    Update script.js

--- Musik/script.js
+++ Musik/script.js
@@ -3,7 +3,7 @@ function sleep(ms) {
 }
 
 let images = [document.getElementById("N1"), document.getElementById("N2"), document.getElementById("N3"), document.getElementById("N4"), document.getElementById("N5"),document.getElementById("N6"),document.getElementById("N7"),document.getElementById("N8"),document.getElementById("N9"),document.getElementById("N10"),document.getElementById("N11"), document.getElementById("N12")];
-async funkction aaa(){
+async function aaa(){
 while (true){
   let i = Math.floor(Math.random()*12);
   images[i].hidden = false;
